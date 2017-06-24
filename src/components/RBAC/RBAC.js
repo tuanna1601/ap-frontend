4d4e5561fb8a8a5f68a60a260246e52400ba0126
Tuan Nguyen <tuanna1601@gmail.com>
2017-06-24 16:51:13 +0700

    fixed RBAC to work on scope

--- src/components/RBAC/RBAC.js
+++ src/components/RBAC/RBAC.js
@@ -2,7 +2,7 @@ import React from 'react';
 import * as _ from 'lodash';
 
 const RBAC = ({ isLoggedIn, user, children, requiredPermissions }) => {
-  if (isLoggedIn && _.intersection(user.permissions, requiredPermissions).length) {
+  if (isLoggedIn && _.intersection(user.scope, requiredPermissions).length) {
     return children;
   }
   return false;
