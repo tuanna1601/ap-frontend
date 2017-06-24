a3c1fcbd4d06816ecd462ced9d8af06faa46d372
Tuan Nguyen <tuanna1601@gmail.com>
2017-06-24 21:04:56 +0700

    added custom filters to UserField

--- src/components/Field/UserFieldContainer.js
+++ src/components/Field/UserFieldContainer.js
@@ -6,7 +6,11 @@ import UserField from './UserField';
 
 const mapStateToProps = (state, ownProps) => ({
   options: _.chain(state.common.users)
-    .filter((user) => ownProps.filterOptions.indexOf(user._id) < 0)
+    .filter((user) => (ownProps.filterOptions ? ownProps.filterOptions.indexOf(user._id) < 0 : true))
+    .filter((user) => (ownProps.userRole ? user.roles.indexOf(ownProps.userRole) > -1 : true))
+    .filter((user) => (ownProps.department ?
+      ownProps.department.reviewers.indexOf(user._id) > -1 :
+      true))
     .map((user) => ({
       value: user._id,
       label: user.name,
