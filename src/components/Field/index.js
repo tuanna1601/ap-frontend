2f90c1947f64f813fdf14b622f33233f5760c9a3
Tuan Nguyen <tuanna1601@gmail.com>
2017-06-29 07:36:49 +0700

    added fields

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -2,10 +2,16 @@ import DepartmentField from './DepartmentFieldContainer';
 import InventoryField from './InventoryFieldContainer';
 import UserField from './UserFieldContainer';
 import CriteriaField from './CriteriaFieldContainer';
+import AdAccountField from './AdAccountFieldContainer';
+import AdCampaignField from './AdCampaignFieldContainer';
+import AdSetField from './AdSetFieldContainer';
 
 export {
   DepartmentField,
   InventoryField,
   UserField,
   CriteriaField,
+  AdAccountField,
+  AdCampaignField,
+  AdSetField,
 };
